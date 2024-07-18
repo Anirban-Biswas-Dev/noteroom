7a0104b720e6fd82d23d3cdfa678c4891e6b71c7
white-fortune <whitefortune271828@gmail.com>
2024-07-18 18:07:49 +0600

    Error fixed, everything is the same as before

--- public/login.js
+++ public/login.js
@@ -6,7 +6,6 @@ socket.emit('connection')
 function sendCred() {
     let username = document.querySelector('.username').value
     let password = document.querySelector('.password').value
-    alert(server_port.port)
 
     socket.emit('send-cred', username, password) // This event (send-cred) will be triggered when the button is clicked
                                                 // And the username and the password will be sent
