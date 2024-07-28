cbae692ecf9074e30b05de403378bc8e9c1835d9
Anirban Biswas <anirban.biswas.biz@gmail.com>
2024-07-28 11:25:02 +0600

    Updated login.js
    Added the js of to see or hide password option

--- public/login.js
+++ public/login.js
@@ -10,3 +10,17 @@ function sendCred() {
     socket.emit('send-cred', username, password) // This event (send-cred) will be triggered when the button is clicked
                                                 // And the username and the password will be sent
 }
+// This snippet will allow users see or hide password they've written
+
+document.addEventListener('DOMContentLoaded', function() {
+    const togglePassword = document.querySelector('.toggle-password');
+    const password = document.querySelector('.password');
+
+    if (togglePassword && password) {
+        togglePassword.addEventListener('click', function() {
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+            this.textContent = this.textContent === 'Show' ? 'Hide' : 'Show';
+        });
+    }
+});
