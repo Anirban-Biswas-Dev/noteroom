9944256d0e47a0c4ae216b01900b348b3916975c
Anirban-Biswas-Dev <anirban.biswas.biz@gmail.com>
2024-08-10 09:11:03 +0600

    minor changes to user profile

--- public/js/user-profile.js
+++ public/js/user-profile.js
@@ -7,4 +7,21 @@ socket.on('no-studentid', function(url) {
     window.location.href = url
 })
 
-// Rafi: Add your javascript here related to ejs views/user-profile.ejs
+// Get the current badge content
+let userBadge = document.querySelector('.top-voice-badge').textContent.trim();
+
+function badgeStyling() {
+    const badgeElement = document.querySelector('.top-voice-badge');
+    // Arnob: see if the html I add conflict with your ejs variables
+    if (userBadge === 'No Badge') {
+        badgeElement.innerHTML = '<div class="top-voice-badge no-badge">No Badge</div>';
+    } else if (userBadge === 'Chemistry') {
+        badgeElement.innerHTML = '<div class="badge-achievement">Chemistry</div>';
+    } else if (userBadge === 'Physics') {
+        badgeElement.innerHTML = '<div class="badge-achievement">Physics</div>';
+    }
+    //will add more later
+}
+
+badgeStyling();
+
