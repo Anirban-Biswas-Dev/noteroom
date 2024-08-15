0d1e67fd6fcd09b0583844d3b8830a3ca5940a81
white-fortune <whitefortune271828@gmail.com>
2024-08-15 08:01:57 +0600

    Slight change in showing featured_notes ammount message

--- public/js/user-profile.js
+++ public/js/user-profile.js
@@ -40,18 +40,4 @@ function badgeStyling() {
     }
 }
 
-function featuredNotesStyling() {
-    const featuredNotesCount = document.querySelector('span.featured-count').textContent.trim() // This will grab the featured notes count from the front-end
-    if(featuredNotesCount == '0') {
-        const featuedNotes = document.querySelector('div.featured-note')
-        featuedNotes.innerHTML = 'No notes to be fetaured' // If there is no featured notes, this message will be shown in that specific div
-        featuedNotes.style.border = '2px solid black' // Like this an example of styling dynamically. The border will be black if there are no featured notes. Develop as you want
-    } else {
-        const featuedNotes = document.querySelector('div.featured-note')
-        featuedNotes.innerHTML = `Featured notes count: ${featuredNotesCount}` // If there is 1+ featured notes, the ammount will be shown
-        featuedNotes.style.border = '2px solid blue' // Like this an example of styling dynamically. Th border will be blue if there are 1+ featured notes
-    }
-}
-
 badgeStyling();
-featuredNotesStyling()
