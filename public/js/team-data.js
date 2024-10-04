45f22e1e1bd7866e2d4088689f45b02d159b0093
Anirban-Biswas-Dev <anirban.biswas.biz@gmail.com>
2024-10-04 11:51:27 +0600

    privacy policy revamped

--- public/js/team-data.js
+++ public/js/team-data.js
@@ -1,7 +1,7 @@
 export const founders = [
     {
         name: 'Anirban Biswas Arnob',
-        imgSrc: '\Anirban Caricature - NoteRoom.png',
+        imgSrc: '\NoteRoom Caricature Cartoonic  - Anirban.jpg.png',
         college: 'Gov.t Chattogram College',
         role: 'The Visionary',
         description: 'Anirban is the mastermind behind the scenes, constantly dreaming up new ideas and envisioning a future in Silicon Valley. A true caffeine-fueled idea machine, he leads the team with a passion for technology, business, and anything that signifies progress. Somehow, he navigates his HSC studies without turning into a robot—at least, not yet! If you catch sight of someone furiously typing away, it’s likely Anirban, deeply immersed in coding or binge-watching WWII movies. And yes, he’s even published a book that at least three people have read!',
@@ -14,7 +14,7 @@ export const founders = [
     },
     {
         name: 'Md. Moshiur Rahman',
-        imgSrc: '\Moshiur Caricature - NoteRoom.png',
+        imgSrc: '\Noteroom Caricature Catoonic - Moshiur.jpg',
         college: 'Gov.t Chattogram College',
         role: 'Backend Boss',
         description: 'Moshiur is our go-to person when the code breaks—and by "go-to," we mean the one everyone calls when panic sets in. With a calm and collected approach, he fixes problems in seconds, often while cracking a dad joke. Known for his cool demeanor in coding chaos, he’s probably debugged more lines of code than most people have had hot dinners. In his spare time, he’s dreaming up ways to automate everything or dominating in Call of Duty. If it moves, he’ll find a way to make it more efficient—some of us are starting to think he might actually be part machine.',
@@ -27,7 +27,7 @@ export const founders = [
     },
     {
         name: 'Devajit Mutsuddy Rohit',
-        imgSrc: '\Devajit NoteRoom Caricature.jpg',
+        imgSrc: '\Rohit Caricature - NoteRoom Cartoonic.jpg',
         college: 'Gov.t City College, Chattogram',
         role: 'The Design Diva',
         description: "Devajit is our creative genius who can turn even the craziest sketches into stunning designs. He speaks fluent hex code, wields Figma like a pro, and gets lost in fonts and color palettes for hours. He’s famous for producing countless “final” versions of design files—just don’t ask how many “final, final” ones there are! He’s constantly obsessing over whether that shade of blue is really blue enough. Perfection is his thing—and the results speak for themselves.",
