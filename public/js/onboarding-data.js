f020dbfcce67ecdd637bbfb5872dd36d02106913
Anirban-Biswas-Dev <anirban.biswas.biz@gmail.com>
2024-12-31 12:50:57 +0600

    Add logos of colleges, Set up template for college selection

---
+++ public/js/onboarding-data.js
@@ -0,0 +1,12 @@
+export const districtCollegeData = {
+    "Dhaka": {
+        "101": { name: "", logo: "" },
+        "102": { name: "", logo: "" },
+        "103": { name: "", logo: "" }
+    },
+    "Chittagong": {
+        "201": { name: "", logo: "" },
+        "202": { name: "", logo: "" }
+    },
+};
+
