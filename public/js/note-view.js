6e44d8387dc046813feee102806f82925293baca
ju4700 <jalal.dev.design@gmail.com>
2025-03-04 00:20:46 +0600

    Add page number display to image modal and adjust carousel control positioning for better visibility

--- public/js/note-view.js
+++ public/js/note-view.js
@@ -611,6 +611,13 @@ function enlargeImage(imageSrc, noteId, initialIndex) {
     <div class="image-modal-content">
       <img src="${imageSrc}" class="enlarged-image" alt="Enlarged Note Image" />
       ${carouselControlsHTML}
+      <div class="image-modal-page-number">
+        <svg class="page-number-icon" width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M14 2H6C5.46957 2 4.96086 2.21071 4.58579 2.58579C4.21071 2.96086 4 3.46957 4 4V20C4 20.5304 4.21071 21.0391 4.58579 21.4142C4.96086 21.7893 5.46957 22 6 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V8L14 2Z" stroke="#1D1B20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M14 2V8H20" stroke="#1D1B20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        <span class="page-number-text">Page ${currentIndex + 1}/${noteImages.length}</span>
+      </div>
       <div class="image-modal-top-controls">
         <button class="image-modal-download" aria-label="Download Image" data-noteid="${noteId}" data-notetitle="${noteTitle}" data-imageindex="${initialIndex}">
           <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -642,6 +649,7 @@ function enlargeImage(imageSrc, noteId, initialIndex) {
   const prevButton = modal.querySelector('.modal-carousel-control.prev');
   const nextButton = modal.querySelector('.modal-carousel-control.next');
   const closeButton = modal.querySelector('.image-modal-close');
+  const pageNumberText = modal.querySelector('.page-number-text'); // Reference to the page number text element
 
   // Function to show the "Link Copied" popup
   function showLinkCopiedPopup() {
@@ -683,6 +691,8 @@ function enlargeImage(imageSrc, noteId, initialIndex) {
     imageElement.src = noteImages[index] || "/images/placeholders/unavailable_content.png";
     downloadButton.setAttribute('data-imageindex', index);
     shareButton.setAttribute('data-imageindex', index);
+    // Update the page number text
+    pageNumberText.textContent = `Page ${index + 1}/${noteImages.length}`;
   }
 
   // Add touch control (swipe gestures) for smaller screens
