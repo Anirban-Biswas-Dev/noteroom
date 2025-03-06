b93e3e6a299562cda07118e6775fddf28abcf56e
white-fortune <whitefortune271828@gmail.com>
2025-03-07 03:13:07 +0600

    Thread-Section basic functionalities and Share Modal fix
    Basic textarea for comment and thread editor
    Adding comments and replies to appropiate places in comment section
    Share Modal noteLink fix for general use

--- frontend/src/pages/post-view/CommentBox.tsx
+++ frontend/src/pages/post-view/CommentBox.tsx
@@ -1,36 +1,39 @@
-import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
-import { ContentEditable } from "@lexical/react/LexicalContentEditable";
-import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
-import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
-import { ListPlugin } from "@lexical/react/LexicalListPlugin";
-import { ListNode, ListItemNode } from "@lexical/list";
+import { useContext, useState } from "react"
+import { CommentsControllerContext } from "./CommentsContainer"
 
-const initialConfig = {
-  namespace: "CommentBox",
-  nodes: [ListNode, ListItemNode],
-  onError: (error: Error) => {},
-};
+export default function CommentEditor() {
+  const {comments: [, setComments]} = useContext(CommentsControllerContext)
+  const [commentData, setCommentData] = useState<string>()
+  function sendComment() {
+    let data = [
+      {
+          _id: Math.random().toString(),
+          noteDocID: "n--no-id--",
+          feedbackContents: `<p>${commentData}</p>`,
+          docType: "feedbacks",
+          commenterDocID: {
+              _id: "--no-id--",
+              profile_pic: "--no-profilepic--",
+              displayname: "Commenter",
+              studentID: "--no-studentid--",
+              username: "--no-username--"
+          },
+          replyCount: 0,
+          upvoteCount: 0,
+          createdAt: "--no-date--",
+          __v: 0,
+          isUpVoted: false
+      },
+      []
+  ]
+    setComments((comments: any) => [...[data], ...comments])
+    setCommentData("")
+  }
 
-function CommentBox() {
   return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <div>
-        <RichTextPlugin
-          contentEditable={<ContentEditable />}
-          ErrorBoundary={({ children }) => <>{children}</>}
-        />
-        <HistoryPlugin />
-        <AutoFocusPlugin />
-        <OnChangePlugin
-          onChange={(editorState) => {}}
-        />
-        <ListPlugin />
-      </div>
-      <button>Send</button>
-    </LexicalComposer>
-  );
+    <>
+      <textarea name="comment-data" value={commentData} onChange={(e) => setCommentData(e.target.value)}></textarea>
+      <button onClick={sendComment}>send</button>
+    </>
+  )
 }
-
-export default CommentBox;
